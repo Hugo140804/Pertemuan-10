d406cca585da78388bb3ca556cbfc4b1ff2ae39a
Hugo140804 <hgugi3nsn@gmail.com>
2026-08-05 10:25:52 +0700

    sinopsis komik.js

--- models/komik.js
+++ models/komik.js
@@ -9,4 +9,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        
+        sinopsis: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
