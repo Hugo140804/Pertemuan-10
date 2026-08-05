c3505e01592af54a6a8ef00091ce955a3bb7ed0c
Hugo140804 <hgugi3nsn@gmail.com>
2026-08-05 10:17:46 +0700

    const genre.js

--- models/genre.js
+++ models/genre.js
@@ -1,1 +1,24 @@
 module.exports = (sequelize, DataTypes) => {
+    const Genre = sequelize.define("Genre", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    }, {
+        tableName: 'genres',
+        timestamps: false
+    });
+
+
+
+    
